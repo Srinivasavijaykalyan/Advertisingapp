6028e98bfb4cbb32cf69eb647cc690e36e328bdc
Srinivasa Vijay Kalyan <87265066+Srinivasavijaykalyan@users.noreply.github.com>
2022-06-30 16:20:02 +0530

    Create Advertising app.java

---
+++ Advertising app.java
@@ -0,0 +1,13 @@
+package com.Advertising.demo.Advertising.App.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Advertisingapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Advertisingapp.class, args);
+	}
+
+}
